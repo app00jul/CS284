c6432637dbd853acadd894920b2dbc32cad9cf0c
Julian Noeske <app00jul@localhost.localdomain>
2022-02-11 11:30:23 -0500

    update IDLList.java

--- src/main/IDLList.java
+++ src/main/IDLList.java
@@ -5,6 +5,74 @@
 
 package main;
 
-public class IDLList<E> {
+import java.util.ArrayList;
 
+public class IDLList<E>{
+    //Inner class
+    private class Node<E>{
+        //Data field
+        private E data;
+        private Node<E> next;
+        private Node<E> prev;
+
+        public Node(E elem){
+            this.data = elem;
+            next = null;
+            prev = null;
+        }
+
+        public Node(E elem, Node<E> next, Node<E> prev) {
+            this.data = elem;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    //Data field
+    private Node<E> head;
+    private Node<E> tail;
+    private int size;
+    private ArrayList<Node<E>> indices;
+
+    public IDLList(){
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+        this.indices = new ArrayList<>();
+    }
+
+    public boolean add(int index, E elem){
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (index == 0) { //Head
+            add(elem);
+        } else {
+            Node<E> current = indices.get(index);
+            Node<E> newCurrent = new Node<>(elem, current, current.prev);
+            current.prev.next = newCurrent;
+            current.prev = newCurrent;
+            size++;
+            indices.add(index, newCurrent);
+        }
+        return true;
+    }
+
+    public boolean add(E elem){
+        if (head == null) { // Empty list
+            head = new Node<E>(elem);
+            tail = head;
+        } else if (head == tail) { // Singleton list
+            head = new Node<E>(elem, tail, null);
+            tail.prev = head;
+        } else {
+            head = new Node<E>(elem, head, null);
+            head.next.prev = head;
+        }
+
+        indices.add(0, head);
+        size++;
+        return true;
+    }
 }
