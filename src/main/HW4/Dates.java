1af07df3d90176c735e9a42f8c2299fe96f67475
app00jul <app00juliann@gmail.com>
2022-04-07 16:39:25 -0400

    minor debug

--- src/main/HW4/Dates.java
+++ src/main/HW4/Dates.java
@@ -1,12 +1,12 @@
 package main.HW4;
 
 public class Dates implements Comparable<Dates>{
-    private int day;
+    private int date;
 
     public int compareTo(Dates Other_Date){
-        if (this.day > Other_Date.day){
+        if (this.date > Other_Date.date){
             return -1;
-        } else if (this.day < Other_Date.day){
+        } else if (this.date < Other_Date.date){
             return 1;
         } else {
             return 0;
---
+++ src/main/HW4/Firstnames.java
@@ -0,0 +1,4 @@
+package main.HW4;
+
+public class Firstnames{
+}
