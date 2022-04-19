5f7dadff48d3303249459c8fbc0734c0669f9548
app00jul <app00juliann@gmail.com>
2022-04-19 11:36:52 -0500

    update shuffling

--- src/main/HW3/Dates.java
+++ src/main/HW3/Dates.java
@@ -15,12 +15,9 @@ public class Dates implements Comparable<Dates>{
 //        randomly generate a year, month and day
 //        range of year: [1000, 2022]
 //        range of month: [1, 12] ([0, 11] for calendar)
-        //LocalDate start_date = LocalDate.of(1000,1,1);
-        //long start = start_date.toEpochDay();
-        //System.out.println(start);
-        Random rand = new Random();
-        int year = rand.nextInt((2022-1000)+1)+1000;
-        int month = rand.nextInt((12-1)+1)+1;
+        LocalDate start_date = LocalDate.of(1000,1,1);
+        long start = start_date.toEpochDay();
+        System.out.println(start);
     }
 
     public String toString() {
